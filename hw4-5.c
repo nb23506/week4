fb2598b85c056b1b664db4d849a7f0103fc1633a
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-10-23 14:08:29 +0900

    Update hw4-5.c

--- hw4-5.c
+++ hw4-5.c
@@ -14,6 +14,7 @@ int main() {
 
     // Compare input weight with ideal weight and give advice
     if (weight > ideal_weight + 5) {
+        printf("Ideal weight = %.6f \n", ideal_weight);
         printf("Let's reduce sweets\n");
     } else if (weight < ideal_weight - 5) {
         printf("Eat some more\n");
