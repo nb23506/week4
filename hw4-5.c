336158af8de89a3a9c20b4f9a73973b5bdc8b34c
nb23506 <167288305+nb23506@users.noreply.github.com>
2024-10-23 16:05:32 +0900

    Update hw4-5.c

--- hw4-5.c
+++ hw4-5.c
@@ -4,9 +4,9 @@ int main() {
     float height, weight, ideal_weight;
 
     // Input user's height and weight
-    printf("Height? \n");
+    printf("Height? ");
     scanf("%f", &height);
-    printf("Weight? \n");
+    printf("Weight? ");
     scanf("%f", &weight);
 
     // Calculate the ideal weight using the formula height * height * 22 / 10000
@@ -14,13 +14,13 @@ int main() {
 
     // Compare input weight with ideal weight and give advice
     if (weight > ideal_weight + 5) {
-        printf("Ideal weight = %.6f \n", ideal_weight);
+        printf("Ideal weight = %.6f\n", ideal_weight);
         printf("Let's reduce sweets\n");
     } else if (weight < ideal_weight - 5) {
-        printf("Ideal weight = %.6f \n", ideal_weight);
+        printf("Ideal weight = %.6f\n", ideal_weight);
         printf("Eat some more\n");
     } else {
-        printf("Ideal weight = %.6f \n", ideal_weight);
+        printf("Ideal weight = %.6f\n", ideal_weight);
         printf("Go for it\n");
     }
 
